78ea16627c71faafc8b8f8b4d7c589856121d2b4
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2021-05-24 21:40:26 +0200

    trying to get dependencies working

--- sender/types.go
+++ sender/types.go
@@ -1,11 +1,38 @@
 package sender
 
-import aggregator "github.com/wwhtrbbtt/PersonalNewsletter/aggregator"
+// import aggregator "github.com/wwhtrbbtt/PersonalNewsletter/aggregator"
+
+type Button struct {
+	Text string `json:"text"` // Button text
+	URL  string `json:"url"`  // Button URL
+}
+
+// Basically the same as a button, but a Module can contain multiple. They are below the text.
+type Link struct {
+	Text string `json:"text"` // Link text
+	URL  string `json:"url"`  // Link URL
+}
+
+type Image struct {
+	Text string `json:"text"`   // Image text
+	URL  string `json:"source"` // Image source URL
+	X    int64  `json:"x"`      // Image X size
+	Y    int64  `json:"y"`      // Image Y size
+}
+
+// The output of a module. Several of these make up an email
+type Module struct {
+	Caption string `json:"caption"` // Title of the module
+	Image   Image  `json:"image"`   // Image of the module - optional
+	Text    string `json:"text"`    // Text of the module - optional
+	Links   []Link `json:"links"`   // Collection of links - optional (ex. RSS feed)
+	Button  Button `json:"button"`  // Button of the module - optional
+}
 
 type Feed struct {
-	Time         string              `json:"time"`         // ex. "10:00"
-	Feedname     string              `json:"feedName"`     // ex. "My email feed"
-	Greetingname string              `json:"greetingName"` // ex. "peet"
-	Email        string              `json:"email"`        // ex. peet@peet.ws
-	Modules      []aggregator.Module `json:"modules"`      // collection of modules
+	Time         string   `json:"time"`         // ex. "10:00"
+	Feedname     string   `json:"feedName"`     // ex. "My email feed"
+	Greetingname string   `json:"greetingName"` // ex. "peet"
+	Email        string   `json:"email"`        // ex. peet@peet.ws
+	Modules      []Module `json:"modules"`      // collection of modules
 }
